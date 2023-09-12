63d9dac8c9d8785adb9fd84458628689d6ccbe8d
larmen9179 <lo.j.armendariz@gmail.com>
2023-09-11 23:12:56 -0600

    Adjusted worldview/randomized maps

--- raycasting.py
+++ raycasting.py
@@ -2,20 +2,36 @@ import numpy as np
 from matplotlib import pyplot as plt
 import keyboard
 
-map = [[1, 1, 1, 1, 1],
-       [1, 0, 0, 0, 1],
-       [1, 0, 1, 0, 1],
-       [1, 0, 0, 0, 1],
-       [1, 1, 1, 1, 1]]
+size = 15
+map = [[list(np.random.uniform(0, 1, 3))] * size for i in range(size)]
+for i in range(size - 2):
+    for j in range(size - 2):
+        if np.random.uniform() > .33:
+            map[i + 1][j + 1] = 0
 
-for i in range(len(map[0])):
-    for j in range(len(map[1])):
-        if map[i][j] == 1:
-            map[i][j] = list(np.random.uniform(0, 1, 3))
-
-posx, posy = (1, 1)
-exitx, exity = (3, 3)
+posx, posy = (1, np.random.randint(1, size - 1))
 rot = np.pi/4
+
+x, y = (posx, posy)
+map[x][y] = 0
+
+count = 0
+while True:
+    testx, testy = (x, y)
+    if np.random.uniform() > 0.5:
+        testx = testx + np.random.choice([-1, 1])
+    else:
+        testy = testy + np.random.choice([-1, 1])
+    if testx > 0 and testx < size - 1 and testy > 0 and testy < size - 1:
+        if map[testx][testy] == 0 or count > 5:
+            count = 0
+            x, y = (testx, testy)
+            map[x][y] = 0
+            if x == size - 2:
+                exitx, exity = (x, y)
+                break
+        else:
+            count = count = 1
 
 while True:
 
@@ -27,27 +43,29 @@ while True:
     for i in range(60):
         rot_i = rot + np.deg2rad(i - 30)
         x, y = (posx, posy)
-        sin, cos = (.02*np.sin(rot_i), .02*np.cos(rot_i))
         n = 0
         while True:
             xx, yy = (x, y)
-            x, y = (x + cos, y + sin)
+            x, y = (x + .02 * np.cos(rot_i), y + .02 * np.sin(rot_i))
             n = n + 1
             
             if abs(int(3*xx)-int(3*x)) > 0 or abs(int(3*yy)-int(3*y)) > 0:
                 tilex.append(i)
-                tiley.append(-1/.02*n)
+                tiley.append(-1/np.sqrt((x-posx)**2 + (y-posy) ** 2) + .0001)
                 if int(x) == exitx and int(y) == exity:
                     tilec.append('b')
                 else:
                     tilec.append('k')
 
             if map[int(x)][int(y)] != 0:
-                h = np.clip(1/(.02*n), 0, 1)
-                c = np.asarray(map[int(x)][int(y)]) * (.3 + .7 * h**2)
+                dist = np.sqrt((x-posx)**2 + (y-posy)**2)
+                h = 1/(dist+.00001)
+                c = np.asarray(map[int(x)][int(y)]) * (.3 + .7/(dist**2 + 1))
                 break
     
         plt.vlines(i, -h, h, lw = 8, colors = c)
+
+    plt.scatter(tilex, tiley, c=tilec)
 
     plt.axis("off"); plt.tight_layout(); plt.axis([0, 60, -1, 1])
     plt.draw(); plt.pause(0.0001); plt.clf()
